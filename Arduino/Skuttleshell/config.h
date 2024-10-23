2088d594bc12437f8f8356f0f06be9609fe13e34
Skuttlebot <101370969+skuttlebot@users.noreply.github.com>
2024-10-23 15:41:07 -0700

    sync

--- Arduino/Skuttleshell/config.h
+++ Arduino/Skuttleshell/config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 
-//#define WIFI_SSID  "PredatorW6-8svG-2.4GHz"
-//#define WIFI_PASSWORD  "ConnectifUK"
-#define WIFI_SSID "OG Cares Labs"
-#define WIFI_PASSWORD "OurGenerationCare$"
+#define WIFI_SSID  "PredatorW6-8svG-2.4GHz"
+#define WIFI_PASSWORD  "ConnectifUK"
+//#define WIFI_SSID "OG Cares Labs"
+//#define WIFI_PASSWORD "OurGenerationCare$"
 #define MODULE  "SkuttleCommandMK1"
 #define ID "SC0001" //Serial Number
 
